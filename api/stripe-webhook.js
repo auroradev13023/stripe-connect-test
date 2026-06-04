6ff827b8e8837db0249776fcf6e97392313955af
Aurora <aurora.dev13023@gmail.com>
2026-06-05 00:59:37 +0700

    ADD: added the webhook

---
+++ api/stripe-webhook.js
@@ -0,0 +1,9 @@
+const Stripe = require('stripe');
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+module.exports = async (req, res) => {
+  res.status(200).json({
+    received: true
+  });
+};
