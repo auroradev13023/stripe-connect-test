7ad0221637613d57ebe76d12fe610abc9c48d5eb
Aurora <aurora.dev13023@gmail.com>
2026-06-05 01:35:29 +0700

    VERIFY: added console ogs for receiving event from stripe

--- api/stripe-webhook.js
+++ api/stripe-webhook.js
@@ -3,6 +3,10 @@ const Stripe = require('stripe');
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 module.exports = async (req, res) => {
+  console.log('Webhook received');
+
+  console.log(JSON.stringify(req.body, null, 2));
+
   res.status(200).json({
     received: true
   });
